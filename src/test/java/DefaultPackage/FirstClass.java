707d1617a8a7efbde068aaf8d4c67bb1f486bf36
Babita <babitamathura@gmail.com>
2020-05-21 15:23:12 +0530

    Code updated

--- src/test/java/DefaultPackage/FirstClass.java
+++ src/test/java/DefaultPackage/FirstClass.java
@@ -2,42 +2,17 @@ package DefaultPackage;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class FirstClass {
-	
-	WebDriver driver = new ChromeDriver ();
-
-	@BeforeTest
-	public void DriverConfiguration()
-	{
-		System.setProperty("webdriver.chrome.driver", "C:/EclipseWorkspace/Drivers/chromedriver.exe");	
-	} 
-	
-	@BeforeMethod
-	public void DeleteCookies()
-	{
-		driver.manage().deleteAllCookies();
-		System.out.println("Cookies deleted...");
-	}
-
-	@Test
-	public void Login()
-	{
-			driver.get("https://www.flipkart.com/");
-			driver.getTitle();
-			
-			//Assert.assertEquals(driver.getTitle(), expected);
-			System.out.println("This is Title of the page : "+ driver.getTitle());
-	}
-	
-	@AfterMethod
-	public void CloseBrowser()
-	{
-		driver.quit();
-		System.out.println("Closed Browser...");
-	}	
+  @Test
+  public void BrowserLaunch() {
+	  
+	  System.setProperty("webdriver.chrome.driver", "C:/EclipseWorkspace/Drivers/chromedriver.exe");
+	  
+	  WebDriver driver = new ChromeDriver();
+	  
+	  driver.get("https://www.flipkart.com/");
+	  
+  }
 }
